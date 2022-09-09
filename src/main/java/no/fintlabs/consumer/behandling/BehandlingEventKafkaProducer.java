e49ab4743d20f8a78514bd5bb83777336e886c41
Trond Magnus Sevre <trond@fintlabs.no>
2022-09-09 15:50:24 +0200

    Update to 3.11, fint-kafka, consumer-shared and some fixes

--- src/main/java/no/fintlabs/consumer/behandling/BehandlingEventKafkaProducer.java
+++ src/main/java/no/fintlabs/consumer/behandling/BehandlingEventKafkaProducer.java
@@ -2,7 +2,9 @@ package no.fintlabs.consumer.behandling;
 
 import no.fintlabs.core.consumer.shared.resource.kafka.EventKafkaProducer;
 import no.fintlabs.kafka.event.EventProducerFactory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BehandlingEventKafkaProducer extends EventKafkaProducer {
     public BehandlingEventKafkaProducer(EventProducerFactory eventProducerFactory, BehandlingConfig behandlingConfig) {
         super(eventProducerFactory, behandlingConfig);
