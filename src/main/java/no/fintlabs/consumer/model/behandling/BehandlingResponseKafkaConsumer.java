29b7ddd0248179e62dd01e6a8966dbdaee639c5e
Hknots <hknots99@gmail.com>
2024-09-02 18:24:45 +0200

    add type to raw parametertized class

--- src/main/java/no/fintlabs/consumer/model/behandling/BehandlingResponseKafkaConsumer.java
+++ src/main/java/no/fintlabs/consumer/model/behandling/BehandlingResponseKafkaConsumer.java
@@ -1,11 +1,12 @@
 package no.fintlabs.consumer.model.behandling;
 
+import no.fint.model.resource.personvern.samtykke.BehandlingResource;
 import no.fintlabs.core.consumer.shared.resource.event.EventResponseKafkaConsumer;
 import no.fintlabs.kafka.event.EventConsumerFactoryService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BehandlingResponseKafkaConsumer extends EventResponseKafkaConsumer {
+public class BehandlingResponseKafkaConsumer extends EventResponseKafkaConsumer<BehandlingResource> {
 
     public BehandlingResponseKafkaConsumer(EventConsumerFactoryService eventConsumerFactoryService, BehandlingConfig behandlingConfig, BehandlingLinker behandlingLinker) {
         super(eventConsumerFactoryService, behandlingConfig, behandlingLinker);
--- src/main/java/no/fintlabs/consumer/model/tjeneste/TjesnesteResponseKafkaConsumer.java
+++ src/main/java/no/fintlabs/consumer/model/tjeneste/TjesnesteResponseKafkaConsumer.java
@@ -1,11 +1,12 @@
 package no.fintlabs.consumer.model.tjeneste;
 
+import no.fint.model.resource.personvern.samtykke.TjenesteResource;
 import no.fintlabs.core.consumer.shared.resource.event.EventResponseKafkaConsumer;
 import no.fintlabs.kafka.event.EventConsumerFactoryService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TjesnesteResponseKafkaConsumer extends EventResponseKafkaConsumer {
+public class TjesnesteResponseKafkaConsumer extends EventResponseKafkaConsumer<TjenesteResource> {
 
     public TjesnesteResponseKafkaConsumer(EventConsumerFactoryService eventConsumerFactoryService, TjenesteConfig tjenesteConfig, TjenesteLinker tjenesteLinker) {
         super(eventConsumerFactoryService, tjenesteConfig, tjenesteLinker);
